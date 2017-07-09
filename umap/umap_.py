ba36480b8726747f302ef644b901acab69c90eed
Leland McInnes <leland.mcinnes@gmail.com>
2017-07-09 11:50:50 -0400

    Add docstrings

--- umap/umap_.py
+++ umap/umap_.py
@@ -5,6 +5,70 @@ from sklearn.base import BaseEstimator
 import numpy as np
 
 class UMAP (BaseEstimator):
+    """Uniform Manifold Approximation and Projection
+
+    Finds a low dimensional embedding of the data that approximates
+    an underlying manifold.
+
+    Parameters
+    ----------
+    n_neighbors: int (optional, default 50)
+        The size of local neighborhood (in terms of number of neighboring
+        sample points) used for manifold approximation. Larger values
+        result in more global views of the manifold, while smaller
+        values result in more local data being preserved. In general
+        values should be in the range 10 to 100.
+
+    n_components: int (optional, default 2)
+        The dimension of the space to embed into. This defaults to 2 to
+        provide easy visualization, but can reasonably be set to any
+        integer value in the range 2 to 100.
+
+    gamma: float (optional, default 1.0)
+        Weighting applied to negative samples in low dimensional embedding
+        optimization. Values higher than one will result in greater weight
+        being given to negative samples.
+
+    n_edge_samples: int (optional, default None)
+        The number of edge/1-simplex samples to be used in optimizing the
+        low dimensional embedding. Larger values result in more accurate
+        embeddings. If None is specified a value will be selected based on
+        the size of the input dataset (typically around dataset_size * 10**4).
+
+    alpha: float (optional, default 1.0)
+        The initial learning rate for the embedding optimization.
+
+    init: string (optional, default 'spectral')
+        How to initialize the low dimensional embedding. Options are:
+            * 'spectral': use a spectral embedding of the fuzzy 1-skeleton
+            * 'random': assign initial emebdding positions at random.
+
+    min_dist: float (optional, default 0.25)
+        The effective minimum distance between embedded points. Smaller values
+        will result in a more clustered/clumped embedding where nearby points
+        on the manifold are drawn closer together, while larger values will
+        result on a more even dispersal of points. The value should be set
+        relative to the ``spread`` value, which determines the scale at which
+        embedded points will be spread out.
+
+    spread: float (optional, default 1.0)
+        The effective scale of embedded points. In combination with ``min_dist``
+        this determines how clustered/clumped the embedded points are.
+
+    a: float (optional, default None)
+        More specific parameters controlling the embedding. If None these
+        values are set automatically as determined by ``min_dist`` and
+        ``spread``.
+    b: float (optional, default None)
+        More specific parameters controlling the embedding. If None these
+        values are set automatically as determined by ``min_dist`` and
+        ``spread``.
+
+    oversampling: int (optional, default 3)
+        The scaling factor for the number of neighbors to sample to attempt
+        to find the local neighborhood in manifold distance. If set to be too
+        large this can have a significant negative impact on performance.
+    """
 
     def __init__(self,
                  n_neighbors=50,
@@ -40,6 +104,11 @@ class UMAP (BaseEstimator):
             self.b = b
 
     def _find_ab_params(self):
+        """Fit a, b params for the differentiable curve used in lower
+        dimensional fuzzy simplicial complex construction. We want the
+        smooth curve (from a pre-defined family with simple gradient) that
+        best matches an offset exponential decay.
+        """
         def curve(x, a, b):
             return 1.0 / (1.0 + a * x ** (2 * b))
 
@@ -53,6 +122,16 @@ class UMAP (BaseEstimator):
         self.b = params[1]
 
     def fit(self, X, y=None):
+        """Fit X into an embedded space.
+
+        Parameters
+        ----------
+        X : array, shape (n_samples, n_features) or (n_samples, n_samples)
+            If the metric is 'precomputed' X must be a square distance
+            matrix. Otherwise it contains a sample per row. If the method
+            is 'exact', X may be a sparse matrix of type 'csr', 'csc'
+            or 'coo'.
+        """
 
         graph = fuzzy_simplicial_set(X, self.n_neighbors, self.oversampling)
 
@@ -73,5 +152,19 @@ class UMAP (BaseEstimator):
         )
 
     def fit_transform(self, X, y=None):
+        """Fit X into an embedded space and return that transformed
+        output.
+
+        Parameters
+        ----------
+        X : array, shape (n_samples, n_features) or (n_samples, n_samples)
+            If the metric is 'precomputed' X must be a square distance
+            matrix. Otherwise it contains a sample per row.
+
+        Returns
+        -------
+        X_new : array, shape (n_samples, n_components)
+            Embedding of the training data in low-dimensional space.
+        """
         self.fit(X)
         return self.embedding_
