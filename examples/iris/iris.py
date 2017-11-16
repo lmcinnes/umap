db59f60cadf30a66d247ea16f6cd140b42c2aa66
Nathaniel Saul <nat@saulgill.com>
2017-11-15 21:41:49 -0800

    a few broken examples from the old api

---
+++ examples/iris/iris.py
@@ -0,0 +1,35 @@
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import CategoricalColorMapper, ColumnDataSource
+from bokeh.palettes import Category10
+
+import umap
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+embedding = umap.UMAP(n_neighbors=50,
+                      alpha=0.5,
+                      init="random",
+                      min_dist=0.001)\
+                .fit_transform(iris.data)
+
+output_file("iris.html")
+
+
+targets = [str(d) for d in iris.target_names]
+
+source = ColumnDataSource(dict(
+    x=[e[0] for e in embedding],
+    y=[e[1] for e in embedding],
+    label=[targets[d] for d in iris.target]
+))
+
+cmap = CategoricalColorMapper(factors=targets, palette=Category10[10])
+
+p = figure(title="Test UMAP on Iris dataset")
+p.circle(x='x',
+         y='y',
+         source=source,
+         color={"field": 'label', "transform": cmap},
+         legend='label')
+
+show(p)
