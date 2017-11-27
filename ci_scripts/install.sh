44a7be857738b6db5eaf3758602737ece8b248bd
Leland McInnes <leland.mcinnes@gmail.com>
2017-11-26 22:21:08 -0500

    Add scikit-learn for faster travis builds

--- ci_scripts/install.sh
+++ ci_scripts/install.sh
@@ -25,7 +25,7 @@ popd
 # Configure the conda environment and put it in the path using the
 # provided versions
 conda create -n testenv --yes python=$PYTHON_VERSION pip nose \
-      numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION numba
+      numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION numba scikit-learn
 
 source activate testenv
 
