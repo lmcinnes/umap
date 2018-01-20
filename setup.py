0f0fab3564ec13677d08c42c4d92b3615e13b5ec
Leland McInnes <leland.mcinnes@gmail.com>
2018-01-20 12:28:29 -0500

    Version change for major layout code changes.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 configuration = {
     'name' : 'umap-learn',
-    'version' : '0.1.5',
+    'version': '0.2.0',
     'description' : 'Uniform Manifold Approximation and Projection',
     'long_description' : readme(),
     'classifiers' : [
