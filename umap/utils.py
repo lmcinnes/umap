cef8831a424fb8ee374b8bbe8c5391e4d7636362
Leland McInnes <leland.mcinnes@gmail.com>
2018-04-08 12:33:40 -0400

    Make heap doesn't need a signature and this might make numba happier

--- umap/utils.py
+++ umap/utils.py
@@ -100,7 +100,7 @@ def rejection_sample(n_samples, pool_size, rng_state):
     return result
 
 
-@numba.njit('f8[:, :, :](i8,i8)')
+@numba.njit()
 def make_heap(n_points, size):
     """Constructor for the numba enabled heap objects. The heaps are used
     for approximate nearest neighbor search, maintaining a list of potential
