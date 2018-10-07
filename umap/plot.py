08047ce64f913fc03bd9618b690aac4dbdf32c18
Leland McInnes <leland.mcinnes@gmail.com>
2018-10-07 15:53:41 -0400

    Start blocking out plot library

---
+++ umap/plot.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+import datashader as ds
+import datashader.transfer_functions as tf
+import matplotlib.pyplot as plt
+
+import sklearn.decomposition
+import sklearn.cluster
+
+
+def points(umap_object):
+    pass
+
+
+def connectivity(umap_object):
+    pass
+
+
+def diagnostic(umap_object):
+    pass
+
+
+def interactive(umap_object):
+    pass
