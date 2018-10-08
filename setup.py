bc354b8c6d9b2179408f016ba701399ebd89b843
Leland McInnes <leland.mcinnes@gmail.com>
2018-10-07 22:18:46 -0400

    Patch: LOBPCG not so good for smaller matrices.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 configuration = {
     'name' : 'umap-learn',
-    'version': '0.3.4',
+    'version': '0.3.5',
     'description' : 'Uniform Manifold Approximation and Projection',
     'long_description' : readme(),
     'classifiers' : [
