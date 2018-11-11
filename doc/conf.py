fcaf57538011e83fae12fdcacb22c3274b6cef24
Leland McInnes <leland.mcinnes@gmail.com>
2018-11-11 16:27:01 -0500

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -96,7 +96,7 @@ theme_navigation_depth = 2
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {'theme_navigation_depth':2}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
