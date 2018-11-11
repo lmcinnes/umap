7a2956c2f95e1f0077133cac10ddf66d3acc50a7
Leland McInnes <leland.mcinnes@gmail.com>
2018-11-11 16:50:18 -0500

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -90,13 +90,18 @@ todo_include_todos = False
 #
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
-theme_navigation_depth = 2
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {'theme_navigation_depth':2}
+html_theme_options = {
+    'navigation_depth': 3,
+    'logo_only': True,
+}
+
+html_logo = "logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
