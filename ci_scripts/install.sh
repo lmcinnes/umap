d038271b997c7f0bd9fdd0cc7c32cd3f5df18fc1
Leland McInnes <leland.mcinnes@gmail.com>
2019-02-13 14:45:38 -0500

    Ensure we have libraries for plotting

--- ci_scripts/install.sh
+++ ci_scripts/install.sh
@@ -28,7 +28,7 @@ if [[ "$DISTRIB" == "conda" ]]; then
   # provided versions
   conda create -n testenv --yes python=$PYTHON_VERSION pip nose \
         numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION numba scikit-learn \
-        holoviews datashader bokeh
+        holoviews datashader bokeh matplotlib
 
   source activate testenv
 
