e33a2879fbafe36c8c29d48042dad8277b068e91
Leland McInnes <leland.mcinnes@gmail.com>
2019-02-13 20:46:50 -0500

    Add more basic plotting tests

--- umap/tests/test_plot.py
+++ umap/tests/test_plot.py
@@ -17,7 +17,15 @@ mapper = umap.UMAP(n_epochs=100).fit(iris.data)
 
 
 def test_plot_runs_at_all():
+    umap.plot.points(mapper)
     umap.plot.points(mapper, labels=iris.target)
     umap.plot.points(mapper, values=iris.data[:, 0])
-    umap.plot.diagnostic(mapper, diagnostic_type="all")
+    umap.plot.points(mapper, theme='fire')
+    umap.plot.diagnostic(mapper, diagnostic_type='all')
+    umap.plot.diagnostic(mapper, diagnostic_type='neighborhood')
     umap.plot.connectivity(mapper)
+    umap.plot.interactive(mapper)
+    umap.plot.interactive(mapper, labels=iris.target)
+    umap.plot.interactive(mapper, values=iris.data[:, 0])
+    umap.plot.interactive(mapper, theme='fire')
+    umap.plot._datashade_points(mapper.embedding_)
