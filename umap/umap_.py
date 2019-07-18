e61812944eed52bdc430774c9cd980114117b0d4
usul83 <ucabgcb@ucl.ac.uk>
2019-07-18 12:06:58 +0100

    convert to Python int (if possible) rather than raise an error

--- umap/umap_.py
+++ umap/umap_.py
@@ -1307,8 +1307,10 @@ class UMAP(BaseEstimator):
         if self.target_n_neighbors < 2 and self.target_n_neighbors != -1:
             raise ValueError("target_n_neighbors must be greater than 2")
         if not isinstance(self.n_components, int):
-            self.n_components = int(self.n_components)
-            #raise ValueError("n_components must be an int")
+            try:
+                self.n_components = int(self.n_components)
+            except ValueError:
+                raise ValueError("n_components must be an int")
         if self.n_components < 1:
             raise ValueError("n_components must be greater than 0")
         if self.n_epochs is not None and (
