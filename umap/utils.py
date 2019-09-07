687d28bc43fc288865f9e38de93022ca51366b22
Leland McInnes <leland.mcinnes@gmail.com>
2019-09-07 15:10:43 -0400

    Back to the old way for now

--- umap/utils.py
+++ umap/utils.py
@@ -27,7 +27,8 @@ def fast_knn_indices(X, n_neighbors):
     """
     knn_indices = np.empty((X.shape[0], n_neighbors), dtype=np.int32)
     for row in numba.prange(X.shape[0]):
-        v = np.argsort(X[row])  # Need to call argsort this way for numba
+        # v = np.argsort(X[row])  # Need to call argsort this way for numba
+        v = X[row].argsort(kind='quicksort')
         v = v[:n_neighbors]
         knn_indices[row] = v
     return knn_indices
