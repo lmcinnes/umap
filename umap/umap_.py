97c9069521b5e97dff04c565095d46c5d4487af1
Andrew Tritt <ajtritt@lbl.gov>
2020-03-12 16:26:20 -0700

    reformat with black

--- umap/umap_.py
+++ umap/umap_.py
@@ -1853,9 +1853,7 @@ class UMAP(BaseEstimator):
                 if y.shape[0] < 4096:
                     try:
                         ydmat = pairwise_distances(
-                            y_,
-                            metric=self.target_metric,
-                            **self._target_metric_kwds
+                            y_, metric=self.target_metric, **self._target_metric_kwds
                         )
                     except (TypeError, ValueError):
                         ydmat = dist.pairwise_special_metric(
@@ -2181,10 +2179,12 @@ class UMAP(BaseEstimator):
         elif self._inverse_distance_func is None:
             raise ValueError("Inverse transform not available for given metric.")
         elif self.n_components >= 8:
-            warn("Inverse transform works best with low dimensional embeddings."
-                 " Results may be poor, or this approach to inverse transform"
-                 " may fail altogether! If you need a high dimensional latent space"
-                 " and inverse transform operations consider using an autoencoder.")
+            warn(
+                "Inverse transform works best with low dimensional embeddings."
+                " Results may be poor, or this approach to inverse transform"
+                " may fail altogether! If you need a high dimensional latent space"
+                " and inverse transform operations consider using an autoencoder."
+            )
 
         X = check_array(X, dtype=np.float32, order="C")
         random_state = check_random_state(self.transform_seed)
