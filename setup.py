dfc59e715f3841eedc801e9caf7abc7db1a6e55e
Leland McInnes <leland.mcinnes@gmail.com>
2020-06-04 21:08:27 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
 
 configuration = {
     "name": "umap-learn",
-    "version": "0.4.3",
+    "version": "0.4.4",
     "description": "Uniform Manifold Approximation and Projection",
     "long_description": readme(),
     "long_description_content_type": "text/x-rst",
