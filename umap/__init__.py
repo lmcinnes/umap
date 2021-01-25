d599e59fe6a6fd5939a3fd7e868658edca1afff0
Leland McInnes <leland.mcinnes@gmail.com>
2021-01-25 10:47:33 -0500

    Fixes for #551

--- umap/__init__.py
+++ umap/__init__.py
@@ -6,7 +6,27 @@ try:
         simplefilter("ignore")
         from .parametric_umap import ParametricUMAP
 except ImportError:
-    warn("Tensorflow not installed; ParametricUMAP will be unavailable")
+    warn(
+        "Tensorflow not installed; ParametricUMAP will be unavailable",
+         category=ImportWarning
+    )
+    # Add a dummy class to raise an error
+    class ParametricUMAP (object):
+
+        def __init__(self, **kwds):
+            warn(
+                """The umap.parametric_umap package requires Tensorflow > 2.0 to be installed.
+            You can install Tensorflow at https://www.tensorflow.org/install
+
+            or you can install the CPU version of Tensorflow using 
+
+            pip install umap-learn[parametric_umap]
+
+            """
+            )
+            raise ImportError(
+                "umap.parametric_umap requires Tensorflow >= 2.0") from None
+
 from .aligned_umap import AlignedUMAP
 
 # Workaround: https://github.com/numba/numba/issues/3341
