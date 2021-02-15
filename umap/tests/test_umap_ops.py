34ecc018fee5f157e92aba25ec453ded59d7343c
Leland McInnes <leland.mcinnes@gmail.com>
2021-02-15 16:19:10 -0500

    Finish fixing test

--- umap/tests/test_umap_ops.py
+++ umap/tests/test_umap_ops.py
@@ -132,10 +132,10 @@ def test_disconnected_data(num_isolates, metric, force_approximation):
         warning_contains = "A large number of your vertices"
     for wn in w:
         flag += warning_contains in str(wn.message)
+
     isolated_vertices = disconnected_vertices(model)
     assert flag == 1, str(([wn.message for wn in w], isolated_vertices))
     # Check that the first isolate has no edges in our umap.graph_
-    isolated_vertices = disconnected_vertices(model)
     assert isolated_vertices[10] == True
     number_of_nan = np.sum(np.isnan(model.embedding_[isolated_vertices]))
     assert number_of_nan >= num_isolates * model.n_components
