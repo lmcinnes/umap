73213d7afa37f72f54135194373165b77fd6f799
Benson Muite <bkmgit@users.noreply.github.com>
2021-06-28 17:17:53 +0300

    add clustering using hdbscan
    follows https://umap-learn.readthedocs.io/en/latest/clustering.html

--- examples/galaxy10sdss.py
+++ examples/galaxy10sdss.py
@@ -2,20 +2,28 @@
 UMAP on the Galaxy10SDSS dataset
 ---------------------------------------------------------
 
-This is an simple example of using UMAP on the Galaxy10SDSS
-dataset. The goal of this example is largely to demonstrate
-the use of supervised learning as an effective tool for
-visualizing and reducing complex data.
+This is an example of using UMAP on the Galaxy10SDSS
+dataset. The goal of this example is largely to 
+demonstrate the use of supervised learning as an 
+effective tool for visualizing and reducing complex data.
+In addition, hdbscan is used to classify the processed
+data.
 """
 
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
 import umap
+import os
 
 # from sklearn.model_selection import train_test_split
 import math
 import requests
+
+# libraries for clustering
+import hdbscan
+import sklearn.cluster as cluster
+from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score
 
 if not os.path.isfile("Galaxy10.h5"):
     url = "http://astro.utoronto.ca/~bovy/Galaxy10/Galaxy10.h5"
@@ -49,11 +57,11 @@ plt.bar(classes, frequency)
 plt.xlabel("Class")
 plt.ylabel("Frequency")
 plt.title("Data Subset")
-plt.savefig("galaxy10_subset.jpg")
+plt.savefig("galaxy10_subset.svg")
 # 2D Embedding
 ## UMAP
 reducer = umap.UMAP(
-    n_components=2, n_neighbors=5, random_state=42, transform_seed=42, verbose=False
+    n_components=2, n_neighbors=20, random_state=42, transform_seed=42, verbose=False
 )
 reducer.fit(X_train)
 
@@ -69,7 +77,7 @@ plt.scatter(
     label=y_train,
 )
 plt.colorbar(boundaries=np.arange(11) - 0.5).set_ticks(np.arange(10))
-plt.savefig("galaxy10_2D_umap.jpg")
+plt.savefig("galaxy10_2D_umap.svg")
 ### UMAP - Supervised
 reducer = umap.UMAP(
     n_components=2, n_neighbors=15, random_state=42, transform_seed=42, verbose=False
@@ -88,7 +96,7 @@ plt.scatter(
     label=y_train,
 )
 plt.colorbar(boundaries=np.arange(11) - 0.5).set_ticks(np.arange(10))
-plt.savefig("galaxy10_2D_umap_supervised.jpg")
+plt.savefig("galaxy10_2D_umap_supervised.svg")
 ### UMAP - Supervised prediction
 galaxy10_umap_supervised_prediction = reducer.transform(X_test)
 fig = plt.figure(1, figsize=(4, 4))
@@ -102,12 +110,48 @@ plt.scatter(
     label=y_test,
 )
 plt.colorbar(boundaries=np.arange(11) - 0.5).set_ticks(np.arange(10))
-plt.savefig("galaxy10_2D_umap_supervised_prediction.jpg")
+plt.savefig("galaxy10_2D_umap_supervised_prediction.svg")
+
+# cluster the data
+labels = hdbscan.HDBSCAN(
+    min_samples=10,
+    min_cluster_size=10,
+).fit_predict(galaxy10_umap_supervised_prediction)
+clustered = labels >= 0
+fig = plt.figure(1, figsize=(4, 4))
+plt.clf()
+plt.scatter(
+    galaxy10_umap_supervised_prediction[~clustered, 0],
+    galaxy10_umap_supervised_prediction[~clustered, 1],
+    color=(0.5, 0.5, 0.5),
+    alpha=0.5,
+)
+plt.scatter(
+    galaxy10_umap_supervised_prediction[clustered, 0],
+    galaxy10_umap_supervised_prediction[clustered, 1],
+    c=y_test[clustered],
+    cmap=plt.cm.nipy_spectral,
+    edgecolor="k",
+    label=y_test[clustered],
+)
+plt.colorbar(boundaries=np.arange(11) - 0.5).set_ticks(np.arange(10))
+plt.savefig("galaxy10_2D_umap_supervised_prediction_clustered.svg")
+
+# Print out information on quality of clustering
+print("2D Supervised Embedding with Clustering")
+print(adjusted_rand_score(y_test, labels), adjusted_mutual_info_score(y_test, labels))
+
+print(
+    adjusted_rand_score(y_test[clustered], labels[clustered]),
+    adjusted_mutual_info_score(y_test[clustered], labels[clustered]),
+)
+
+print(np.sum(clustered) / y_test.shape[0])
 
 # 3D Embedding
 ## UMAP
 reducer = umap.UMAP(
-    n_components=3, n_neighbors=5, random_state=42, transform_seed=42, verbose=False
+    n_components=3, n_neighbors=20, random_state=42, transform_seed=42, verbose=False
 )
 reducer.fit(X_train)
 galaxy10_umap = reducer.transform(X_train)
@@ -124,10 +168,10 @@ p = ax.scatter(
     label=y_train,
 )
 fig.colorbar(p, ax=ax, boundaries=np.arange(11) - 0.5).set_ticks(np.arange(10))
-plt.savefig("galaxy10_3D_umap.jpg")
+plt.savefig("galaxy10_3D_umap.svg")
 ## UMAP - Supervised
 reducer = umap.UMAP(
-    n_components=3, n_neighbors=15, random_state=42, transform_seed=42, verbose=False
+    n_components=3, n_neighbors=20, random_state=42, transform_seed=42, verbose=False
 )
 reducer.fit(X_train, y_train)
 galaxy10_umap_supervised = reducer.transform(X_train)
@@ -144,7 +188,7 @@ p = ax.scatter(
     label=y_train,
 )
 fig.colorbar(p, ax=ax, boundaries=np.arange(11) - 0.5).set_ticks(np.arange(10))
-plt.savefig("galaxy10_3D_umap_supervised.jpg")
+plt.savefig("galaxy10_3D_umap_supervised.svg")
 ## UMAP - Supervised prediction
 galaxy10_umap_supervised_prediction = reducer.transform(X_test)
 fig = plt.figure(1, figsize=(4, 4))
@@ -160,4 +204,72 @@ p = ax.scatter(
     label=y_test,
 )
 fig.colorbar(p, ax=ax, boundaries=np.arange(11) - 0.5).set_ticks(np.arange(10))
-plt.savefig("galaxy10_3D_umap_supervised_prediction.jpg")
+plt.savefig("galaxy10_3D_umap_supervised_prediction.svg")
+
+# cluster the data
+labels = hdbscan.HDBSCAN(
+    min_samples=10,
+    min_cluster_size=10,
+).fit_predict(galaxy10_umap_supervised_prediction)
+clustered = labels >= 0
+fig = plt.figure(1, figsize=(4, 4))
+plt.clf()
+ax = fig.add_subplot(projection="3d")
+q = ax.scatter(
+    galaxy10_umap_supervised_prediction[~clustered, 0],
+    galaxy10_umap_supervised_prediction[~clustered, 1],
+    galaxy10_umap_supervised_prediction[~clustered, 2],
+    color=(0.5, 0.5, 0.5),
+    alpha=0.5,
+)
+p = ax.scatter(
+    galaxy10_umap_supervised_prediction[clustered, 0],
+    galaxy10_umap_supervised_prediction[clustered, 1],
+    galaxy10_umap_supervised_prediction[clustered, 2],
+    c=y_test[clustered],
+    cmap=plt.cm.nipy_spectral,
+    edgecolor="k",
+    label=y_test[clustered],
+)
+fig.colorbar(p, ax=ax, boundaries=np.arange(11) - 0.5).set_ticks(np.arange(10))
+plt.savefig("galaxy10_3D_umap_supervised_prediction_clustered.svg")
+
+# Print out information on quality of clustering
+print("3D Supervised Embedding with Clustering")
+print(adjusted_rand_score(y_test, labels), adjusted_mutual_info_score(y_test, labels))
+
+print(
+    adjusted_rand_score(y_test[clustered], labels[clustered]),
+    adjusted_mutual_info_score(y_test[clustered], labels[clustered]),
+)
+
+print(np.sum(clustered) / y_test.shape[0])
+## Dimensions 4 to 25
+for dimensions in range(4, 26):
+    reducer = umap.UMAP(
+        n_components=dimensions,
+        n_neighbors=20,
+        random_state=42,
+        transform_seed=42,
+        verbose=False,
+    )
+    reducer.fit(X_train, y_train)
+    galaxy10_umap_supervised = reducer.transform(X_train)
+    ## UMAP - Supervised prediction
+    galaxy10_umap_supervised_prediction = reducer.transform(X_test)
+    # cluster the data
+    labels = hdbscan.HDBSCAN(
+        min_samples=10,
+        min_cluster_size=10,
+    ).fit_predict(galaxy10_umap_supervised_prediction)
+    clustered = labels >= 0
+    # Print out information on quality of clustering
+    print(str(dimensions) + "D Supervised Embedding with Clustering")
+    print(
+        adjusted_rand_score(y_test, labels), adjusted_mutual_info_score(y_test, labels)
+    )
+    print(
+        adjusted_rand_score(y_test[clustered], labels[clustered]),
+        adjusted_mutual_info_score(y_test[clustered], labels[clustered]),
+    )
+    print(np.sum(clustered) / y_test.shape[0])
