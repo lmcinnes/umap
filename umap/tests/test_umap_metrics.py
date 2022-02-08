28bea09f0f6a6b3a200ee038e97371936002becb
Leland McInnes <leland.mcinnes@gmail.com>
2022-02-08 15:11:20 -0500

    Fixes for weighted minkowski (per changes in SciPy); Scipy changed the name...

--- umap/tests/test_umap_metrics.py
+++ umap/tests/test_umap_metrics.py
@@ -493,24 +493,25 @@ def test_grad_metrics_match_metrics(spatial_data, spatial_distances):
         err_msg="Distances don't match " "for metric seuclidean",
     )
 
-    # Weighted minkowski
-    dist_matrix = pairwise_distances(spatial_data, metric="wminkowski", w=v, p=3)
-    test_matrix = np.array(
-        [
+    if scipy_full_version >= "1.8":
+        # Weighted minkowski
+        dist_matrix = pairwise_distances(spatial_data, metric="minkowski", w=v, p=3)
+        test_matrix = np.array(
             [
-                dist.weighted_minkowski_grad(spatial_data[i], spatial_data[j], v, p=3)[
-                    0
+                [
+                    dist.weighted_minkowski_grad(spatial_data[i], spatial_data[j], v, p=3)[
+                        0
+                    ]
+                    for j in range(spatial_data.shape[0])
                 ]
-                for j in range(spatial_data.shape[0])
+                for i in range(spatial_data.shape[0])
             ]
-            for i in range(spatial_data.shape[0])
-        ]
-    )
-    assert_array_almost_equal(
-        test_matrix,
-        dist_matrix,
-        err_msg="Distances don't match " "for metric weighted_minkowski",
-    )
+        )
+        assert_array_almost_equal(
+            test_matrix,
+            dist_matrix,
+            err_msg="Distances don't match " "for metric weighted_minkowski",
+        )
 
     # Mahalanobis
     v = np.abs(np.random.randn(spatial_data.shape[1], spatial_data.shape[1]))
