6790d615bfe8544961f60e83056bf28bdec5b6d7
James Melville <jlmelville@gmail.com>
2022-09-05 10:44:51 -0700

    match install requires to requirements.txt

--- setup.py
+++ setup.py
@@ -45,11 +45,11 @@ configuration = {
     "packages": ["umap"],
     "install_requires": [
         "numpy >= 1.17",
+        "scipy >= 1.3.1",
         "scikit-learn >= 0.22",
-        "scipy >= 1.0",
-        "numba >= 0.49",
+        "numba >= 0.51.2",
         "pynndescent >= 0.5",
-        "tqdm",
+        "tbb >= 2019.0" "tqdm",
     ],
     "extras_require": {
         "plot": [
