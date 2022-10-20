f050a97c7a1e2a7fd17aaa3dd2f683f8f1bd08f8
Jonathan Conder <jonathan.conder@soulmachines.com>
2022-10-20 14:18:00 +1300

    Avoid calling should_pickle on models

--- umap/parametric_umap.py
+++ umap/parametric_umap.py
@@ -483,7 +483,7 @@ class ParametricUMAP(UMAP):
         return dict(
             (k, v)
             for (k, v) in self.__dict__.items()
-            if should_pickle(k, v) and k not in ("optimizer", "encoder", "decoder", "parametric_model")
+            if k not in ("optimizer", "encoder", "decoder", "parametric_model") and should_pickle(k, v)
         )
 
     def save(self, save_location, verbose=True):
