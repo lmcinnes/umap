6d5a5341f0887229d8656087b417c1be889a55e8
Samantha Klier <samantha.klier@unibw.de>
2023-03-01 11:16:17 +0100

    Updated test for data input checks

--- umap/tests/test_data_input.py
+++ umap/tests/test_data_input.py
@@ -1,23 +1,17 @@
 import numpy as np
 import pytest as pytest
-
-# Check Data Input
-# -----------------------
 from numba import njit
-
 from umap import UMAP
 
 
 @pytest.fixture(scope="session")
 def all_finite_data():
-    d = np.arange(100.0).reshape(25, 4)
-    return d
+    return np.arange(100.0).reshape(25, 4)
 
 
 @pytest.fixture(scope="session")
 def inverse_data():
-    d = np.arange(50).reshape(25, 2)
-    return d
+    return np.arange(50).reshape(25, 2)
 
 
 @njit
@@ -28,28 +22,64 @@ def nan_dist(a: np.ndarray, b: np.ndarray):
 
 
 def test_check_input_data(all_finite_data, inverse_data):
+    """
+    Data input to UMAP gets checked for liability.
+    This tests checks the if data input is dismissed/accepted
+    according to the "force_all_finite" keyword as used by
+    sklearn.
+
+    Parameters
+    ----------
+    all_finite_data
+    inverse_data
+    -------
+
+    """
     inf_data = all_finite_data.copy()
     inf_data[0] = np.inf
-
     nan_data = all_finite_data.copy()
     nan_data[0] = np.nan
+    inf_nan_data = all_finite_data.copy()
+    inf_nan_data[0] = np.nan
+    inf_nan_data[1] = np.inf
 
-    u = UMAP(metric=nan_dist)
+    # wrapper to call each data handling function of UMAP in a convenient way
+    def call_umap_functions(data, force_all_finite):
+        u = UMAP(metric=nan_dist)
+        if force_all_finite is None:
+            u.fit_transform(data)
+            u.fit(data)
+            u.transform(data)
+            u.update(data)
+            u.inverse_transform(inverse_data)
+        else:
+            u.fit_transform(data, force_all_finite=force_all_finite)
+            u.fit(data, force_all_finite=force_all_finite)
+            u.transform(data, force_all_finite=force_all_finite)
+            u.update(data, force_all_finite=force_all_finite)
+            u.inverse_transform(inverse_data)
 
-    u.fit_transform(all_finite_data)
-    u.fit(all_finite_data)
-    u.transform(all_finite_data)
-    u.update(all_finite_data)
-    u.inverse_transform(inverse_data)
+    # Check whether correct data input is accepted
+    call_umap_functions(all_finite_data, None)
+    call_umap_functions(all_finite_data, True)
 
-    u.fit_transform(nan_data, force_all_finite='allow-nan')
-    u.fit(nan_data, force_all_finite='allow-nan')
-    u.transform(nan_data, force_all_finite='allow-nan')
-    u.update(nan_data, force_all_finite='allow-nan')
-    u.inverse_transform(inverse_data)
+    call_umap_functions(nan_data, 'allow-nan')
+    call_umap_functions(all_finite_data, 'allow-nan')
 
-    u.fit_transform(inf_data, force_all_finite=False)
-    u.fit(inf_data, force_all_finite=False)
-    u.transform(inf_data, force_all_finite=False)
-    u.update(inf_data, force_all_finite=False)
-    u.inverse_transform(inverse_data)
+    call_umap_functions(inf_data, False)
+    call_umap_functions(inf_nan_data, False)
+    call_umap_functions(nan_data, False)
+    call_umap_functions(all_finite_data, False)
+
+    # Check whether illegal data raises a ValueError
+    with pytest.raises(ValueError):
+        call_umap_functions(nan_data, None)
+        call_umap_functions(inf_data, None)
+        call_umap_functions(inf_nan_data, None)
+
+        call_umap_functions(nan_data, True)
+        call_umap_functions(inf_data, True)
+        call_umap_functions(inf_nan_data, True)
+
+        call_umap_functions(inf_data, 'allow-nan')
+        call_umap_functions(inf_nan_data, 'allow-nan')
