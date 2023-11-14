c427b48c02ef9216649aa1032de8c4257296e670
Louis Lac <louis.lac@neovision.fr>
2023-11-14 13:41:58 +0100

    Fix pkg_resources deprecation warning
    Use importlib instead of pkg_resource.

--- umap/__init__.py
+++ umap/__init__.py
@@ -33,9 +33,9 @@ from .aligned_umap import AlignedUMAP
 # Workaround: https://github.com/numba/numba/issues/3341
 import numba
 
-import pkg_resources
+from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = pkg_resources.get_distribution("umap-learn").version
-except pkg_resources.DistributionNotFound:
+    __version__ = version("umap-learn")
+except PackageNotFoundError:
     __version__ = "0.5-dev"
