38f136713487f03ff6f9fd8f8cc45e9643e86caf
AMS-Hippo <108019025+AMS-Hippo@users.noreply.github.com>
2024-08-16 22:35:03 -0400

    Update parametric_umap.py
    More pep8 fixes.

--- umap/parametric_umap.py
+++ umap/parametric_umap.py
@@ -45,7 +45,7 @@ except ImportError:
     warn(
         """Torch and ONNX required for exporting to those formats."""
     )
-    torch_imported=False
+    torch_imported = False
 
 
 class ParametricUMAP(UMAP):
@@ -360,9 +360,9 @@ class ParametricUMAP(UMAP):
         # Extract encoder
         km = self.encoder
         # Extract weights
-        pm = PumapNet(self.dims[0],self.n_components)
-        pm = weight_copier(km,pm)
-        
+        pm = PumapNet(self.dims[0], self.n_components)
+        pm = weight_copier(km, pm)
+
         # Put in ONNX
         dummy_input = torch.randn(1, self.dims[0])
         # Invoke export
@@ -1054,17 +1054,18 @@ class UMAPModel(keras.Model):
 if torch_imported:
 
     class PumapNet(nn.Module):
-        def __init__(self,indim,outdim):
+        
+        def __init__(self, indim, outdim):
             super(PumapNet, self).__init__()
-            self.dense1 = nn.Linear(indim,100)
-            self.dense2 = nn.Linear(100,100)
-            self.dense3 = nn.Linear(100,100)
-            self.dense4 = nn.Linear(100,outdim)
+            self.dense1 = nn.Linear(indim, 100)
+            self.dense2 = nn.Linear(100, 100)
+            self.dense3 = nn.Linear(100, 100)
+            self.dense4 = nn.Linear(100, outdim)
     
             """
             Creates the same network as the one used by parametric UMAP.
             Note: shape of network is fixed.
-    
+
             Parameters
             ----------
             indim : int
@@ -1072,7 +1073,7 @@ if torch_imported:
             outdim : int
                 dimension of output of network.
             """
-    
+
         def forward(self, x):
             x = self.dense1(x)
             x = F.relu(x)
@@ -1083,12 +1084,12 @@ if torch_imported:
             x = self.dense4(x)
             x = F.relu(x)
             return x
-    
+
     ######################
     # 2. Copying weights #
     ######################
-    
-    def weight_copier(km,pm):
+
+    def weight_copier(km, pm):
         """ Copies weights from a parametric UMAP encoder to pytorch.
         Parameters
         ----------
@@ -1100,22 +1101,22 @@ if torch_imported:
             Net with copied weights.
         """
         kweights = km.get_weights()
-        n_layers = int(len(kweights)/2) # The actual number of layers
-    
+        n_layers = int(len(kweights)/2)  # The actual number of layers
+
         # Get the names of the pytorch layers
         all_keys = [x for x in pm.state_dict().keys()]
         pm_names = [all_keys[2*i].split(".")[0] for i in range(4)]
-    
+
         # Set a variable for the state dict
         pyt_state_dict = pm.state_dict()
-    
-        for i in range(n_layers): # Set weights and biases in the dict.
+
+        for i in range(n_layers): #  Set weights and biases in the dict.
             pyt_state_dict[pm_names[i] + ".bias"] = kweights[2*i + 1]
             pyt_state_dict[pm_names[i] + ".weight"] = np.transpose(kweights[2*i])
-    
+
         for key in pyt_state_dict.keys():
             pyt_state_dict[key] = torch.from_numpy(pyt_state_dict[key])
-    
+
         # Update
         pm.load_state_dict(pyt_state_dict)
         return pm
